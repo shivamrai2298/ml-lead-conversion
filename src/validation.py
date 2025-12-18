eadada76d1ae5256551b1fbb83e0c9dafc3c86a9
Shivam Rai <shivamrai2298@gmail.com>
2025-12-18 16:54:06 +0530

    Update validation.py

--- src/validation.py
+++ src/validation.py
@@ -1,1 +1,10 @@
+import pandas as pd
+
+def validate(df):
+    assert set(["converted"]).issubset(df.columns)
+    assert df["converted"].isin([0,1]).all()
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/processed/data.csv")
+    validate(df)
 
